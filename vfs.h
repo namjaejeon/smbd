f58dfbe5793212038475878ffe33fdff335f97ef
Fredrik Ternerot <fredrikt@axis.com>
2020-04-22 08:23:35 +0200

    ksmbd: add error handling for kmalloc xattr_stream_name
    Signed-off-by: Fredrik Ternerot <fredrikt@axis.com>
    Signed-off-by: Namjae Jeon <namjae.jeon@samsung.com>

--- vfs.h
+++ vfs.h
@@ -187,7 +187,8 @@ int ksmbd_vfs_fsetxattr(const char *filename,
 			int flags);
 
 int ksmbd_vfs_xattr_stream_name(char *stream_name,
-				char **xattr_stream_name);
+				char **xattr_stream_name,
+				size_t *xattr_stream_name_size);
 
 int ksmbd_vfs_truncate_xattr(struct dentry *dentry, int wo_streams);
 int ksmbd_vfs_remove_xattr(struct dentry *dentry, char *attr_name);
